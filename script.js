c1873795a95d9de804410b3244faed0478a65d7c
Shada <43104909+shada12@users.noreply.github.com>
2022-11-07 12:50:00 -0500

    edited

--- script.js
+++ script.js
@@ -1,102 +1,87 @@
-var valid = true;
-
 function validateForm() {
-
     // let password = document.forms["myForm"]["password"].value;
+    let firstName = document.getElementById('first_name').value;
+    let lastName = document.getElementById('last_name').value;
+    let email = document.getElementById('email').value;
+    let phone = document.getElementById('phone').value;
     let password = document.getElementById("password").value;
     let confirmPassword = document.getElementById("confirm_password").value;
 
-    console.log(password);
-    console.log(confirmPassword);
+    if (password === "") {
+        document.getElementById("passwordAlert").innerHTML = "Password is required";
+    }
+    else if (password.length >= 6) {
+        document.getElementById("passwordAlert").innerHTML = "";
+    }
+    else {
+        document.getElementById('passwordAlert').innerHTML = "Password length should be 6 or more";
+    }
 
-    if (password == "" || confirmPassword == "") {
+    if (confirmPassword === "") {
+        document.getElementById("confirmPasswordAlert").innerHTML = "Confirm password is required";
 
-        if (password == "") {
-            document.getElementById("passwordAlert").innerHTML = "Password is required";
-            valid = false;
-        } else {
-            document.getElementById('passwordAlert').innerHTML = "";
-        }
+    } else if (confirmPassword.length >= 6) {
+        document.getElementById('confirmPasswordAlert').innerHTML = "";
+    }
+    else {
+        document.getElementById('confirmPasswordAlert').innerHTML = "Password length should be 6 or more";
+    }
 
-        if (confirmPassword == "") {
-            document.getElementById("confirmPasswordAlert").innerHTML = "Confirm password is required";
-            valid = false;
+    if (password.length >= 6 && confirmPassword.length >= 6) {
+        if (password === confirmPassword) {
+            document.getElementById("passwordAlert").innerHTML = "";
+            document.getElementById("confirmPasswordAlert").innerHTML = "";
+            document.getElementById("password").style.border = "1px solid gray";
+            document.getElementById("confirm_password").style.border = "1px solid gray";
         }
         else {
-            document.getElementById('confirmPasswordAlert').innerHTML = "";
+            document.getElementById("passwordAlert").innerHTML = "*Passwords do not match";
+            document.getElementById("confirmPasswordAlert").innerHTML = "*Passwords do not match";
+            document.getElementById("password").style.border = "2px solid red";
+            document.getElementById("confirm_password").style.border = "2px solid red";
         }
-
-    } else if (password !== confirmPassword) {
-
-        document.getElementById("passwordAlert").innerHTML = "*Passwords do not match";
-        document.getElementById("confirmPasswordAlert").innerHTML = "";
-        document.getElementById("password").style.border = "2px solid red";
-        document.getElementById("confirm_password").style.border = "2px solid red";
-        valid = false;
-
-    } else if (password === confirmPassword) {
-        document.getElementById("passwordAlert").innerHTML = "";
-        document.getElementById("confirmPasswordAlert").innerHTML = "";
-        document.getElementById("password").style.border = "1px solid gray";
-        document.getElementById("confirm_password").style.border = "1px solid gray";
     }
 
-    if (document.getElementById('first_name').value == "") {
+    if (firstName === "") {
         document.getElementById('fnameAlert').innerHTML = "First name is required";
-        valid = false;
     } else {
         document.getElementById('fnameAlert').innerHTML = "";
-
     }
-
-    if (document.getElementById('last_name').value == "") {
+    if (lastName === "") {
         document.getElementById('lnameAlert').innerHTML = "Last name is required";
-        valid = false;
     } else {
         document.getElementById('lnameAlert').innerHTML = "";
     }
 
-                          /*********Email***** */
     var mailformat = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
-    if (document.getElementById('email').value == "") {
+    if (email === "") {
 
         document.getElementById('emailAlert').innerHTML = "Email is required";
-        valid = false;
-    } else if (document.getElementById('email').value != "") {
+    } else {
 
-        if (document.getElementById('email').value.match(mailformat)) {
+        if (email.match(mailformat)) {
             document.getElementById("emailAlert").innerHTML = "";
         }
         else {
             document.getElementById("emailAlert").innerHTML = "Invalid email address";
-            valid = false;
         }
     }
 
-                         /*********Phone****** */
-
     var phoneformat = /^\+(?:[0-9] ?){6,14}[0-9]$/;
-
-    if (document.getElementById('phone').value == "") {
+    if (phone === "") {
 
         document.getElementById('phoneAlert').innerHTML = "Phone is required";
-        valid = false;
-    } else if (document.getElementById('phone').value != "") {
+    } else {
 
-        if (document.getElementById('phone').value.match(phoneformat)) {
+        if (phone.match(phoneformat)) {
             document.getElementById("phoneAlert").innerHTML = "";
         }
         else {
             document.getElementById("phoneAlert").innerHTML = "Invalid phone number";
-            valid = false;
         }
-
     }
-}   
-                           /*********ResetBtn**********/
-
+}
 document.getElementById("resetBtn").addEventListener("click", function () {
-
     document.getElementById("password").style.border = "1px solid gray";
     document.getElementById("confirm_password").style.border = "1px solid gray";
     document.getElementById("passwordAlert").innerHTML = "";
@@ -105,5 +90,5 @@ document.getElementById("resetBtn").addEventListener("click", function () {
     document.getElementById('fnameAlert').innerHTML = "";
     document.getElementById("emailAlert").innerHTML = "";
     document.getElementById("phoneAlert").innerHTML = "";
-
 });
+
